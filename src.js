b9d430f0676f014bfc2f568d9ccb34b22b4e0029
Jenny Zhang <42523645+cowjuh@users.noreply.github.com>
2020-04-10 23:37:01 -0700

    Created simple showcase website

---
+++ src.js
@@ -0,0 +1,15 @@
+function opacityLow(x) {
+    x.style.opacity = "50%";
+}
+
+function opacityFull(x) {
+    x.style.opacity = "100%";
+}
+
+function turnPink(x) {
+    x.style.color = "#FEBCFD";
+}
+
+function turnWhite(x) {
+    x.style.color = "#FFF";
+}
